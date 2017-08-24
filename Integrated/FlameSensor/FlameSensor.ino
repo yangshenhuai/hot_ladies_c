0d05272fa032778882ef337cd86e1e0b7ddcaad5
peter_young <yangshenhuai2008@163.com>
2017-08-24 21:51:06 +0800

    add flame sensor

--- Integrated/FlameSensor/FlameSensor.ino
+++ Integrated/FlameSensor/FlameSensor.ino
@@ -1,14 +1,39 @@
+/* Flame Sensor analog example.
+Code by Reichenstein7 (thejamerson.com)
 
-float fire_sensor = A1;
+Flame Sensor Ordered from DX ( http://www.dx.com/p/arduino-flame-sensor-for-temperature-
+detection-blue-dc-3-3-5v-118075?tc=USD&gclid=CPX6sYCRrMACFZJr7AodewsA-Q#.U_n5jWOrjfU )
+
+To test view the output, point a serial monitor such as Putty at your arduino. 
+*/
+
+// lowest and highest sensor readings:
+const int sensorMin = 0;     // sensor minimum
+const int sensorMax = 1024;  // sensor maximum
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(fire_sensor,INPUT);
-  Serial.begin(9600);
+  // initialize serial communication @ 9600 baud:
+  Serial.begin(9600);  
+}
+void loop() {
+  // read the sensor on analog A0:
+  int sensorReading = analogRead(A1);
+  // map the sensor range (four options):
+  // ex: 'long int map(long int, long int, long int, long int, long int)'
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+  
+  // range value:
+  switch (range) {
+  case 0:    // A fire closer than 1.5 feet away.
+    Serial.println("** Close Fire **");
+    break;
+  case 1:    // A fire between 1-3 feet away.
+    Serial.println("** Distant Fire **");
+    break;
+  case 2:    // No fire detected.
+    Serial.println("No Fire");
+    break;
+  }
+  delay(1);  // delay between reads
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-   float fire_value = analogRead(fire_sensor);
-   Serial.println(fire_value);
-}
--- Integrated/Integrated.ino
+++ Integrated/Integrated.ino
@@ -1,11 +1,23 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
+
+#define DHTPIN 3
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN,DHTTYPE);
 /**
  * will try to read from several sensors
  * 
  */
 
 float gas_sensor = A0;
-int i = 0; // use the i as the indicator to determin read from which digil output or analogue out
+float flame_sensor = A1;
+float temp ;
+float hum ;
+int i = 0 ; // use the i as the indicator to determin read from which digil output or analogue out
+const int flameSensorMin = 0;
+const int flameSensorMax = 1024;
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,12 +31,26 @@ void loop() {
     i = 0 ;// avoid overflow 
   }
 
- 
-  Serial.print("gs");
-  Serial.print(readGasSensor());
-  Serial.println("ge");
-
-
+  if(i % 3==0) {
+    Serial.print("gs");
+    Serial.print(readGasSensor());
+    Serial.println("ge");
+  } else if(i%3==1)  {
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+    Serial.print("hts");
+    Serial.print(hum);
+    Serial.print("-");
+    Serial.print(temp);
+    Serial.println("hte");
+  } else {
+    int range = detectFire();
+    if(range == 0 || range ==1) {
+      Serial.println("Fire");
+    } else {
+      Serial.println("No F");
+    }
+  }
   i++;
 }
 
@@ -32,4 +58,11 @@ float readGasSensor() {
   float gas_value=analogRead(gas_sensor);
   return gas_value;
 }
+
+int detectFire(){
+  int flameSensorReading = analogRead(flame_sensor);
+  int range = map(flameSensorReading,flameSensorMin,flameSensorMax,0,3);
+  return range; 
+}
+
 
